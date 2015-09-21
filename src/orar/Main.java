b553e7ce90e2b2f27128e64c3b1c430e5a89634c
Inonut <raducanu_dragos12@yahoo.com>
2015-09-21 22:36:50 +0300

    test commit

--- src/orar/Main.java
+++ src/orar/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		
+		//test commit
 		OrarSevices orarSevices = new OrarServicesImpl();
 		OrarDao orarDao = new OrarDaoImpl();
 		
