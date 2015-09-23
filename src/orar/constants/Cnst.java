545a5cf71d18e8731feedc1429bce338aed6c778
Catalin <danescucatalinconstantin@yahoo.ro>
2015-09-23 14:05:09 -0700

    in cnst

--- src/orar/constants/Cnst.java
+++ src/orar/constants/Cnst.java
@@ -5,7 +5,12 @@ import javafx.scene.paint.Color;
 public class Cnst {
 	
 	public static final String URL = "C:/Users/Danescu/git/Orar/orarDB.txt";
-//dddd
+//ddddd sa mai caut pe net de eroare
+	//ok, tadaaaaaa :) )CE )avea?? incercai sa postezi la mine direct si asta nu se poate 
+	// acu e un pic mai clar de ce e acel push
+	// tu practic nu postezi direct la mine in repository ci in unu de test, daca eu (autorul) vreau schimbarea iii dau curs daca nu o ignor fara sa imi afevteze codulam inteles ;) acu sa vada daca a ajuns si la mine:)
+	//unde ai modificat?
+	//nu mai e :r e D da
 	public static String getColorFormat(Color color){
 		return String.format( "#%02X%02X%02X",
 	            (int)( color.getRed() * 255 ),
